38f58e57488ce0dd419e4a8326cef01015acad69
gregborane <madaradellac@gmail.com>
2026-05-16 14:39:41 -0400

    changed args to fixed values

--- lua/notebook-navigator/repls.lua
+++ lua/notebook-navigator/repls.lua
@@ -65,7 +65,7 @@ repls.pyrepl = function(start_line, end_line, repl_args, cell_marker)
   local main_pyrepl = require "pyrepl"
 
   if not main_pyrepl.get_chan() then
-    main_pyrepl.open_repl({ repl_args })
+    main_pyrepl.open_repl({ "--kernel", "python3" })
   end
 
   main_pyrepl.send_cell()
